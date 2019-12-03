c750c3bde2bc00ddf7af4a4c652c629bb1480906
深瀬 貴将 <fukase@arsaga.jp>
2019-12-03 12:29:16 +0900

    Initial Commit

---
+++ APIpractice2Tests/APIpractice2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  APIpractice2Tests.swift
+//  APIpractice2Tests
+//
+//  Created by 深瀬 貴将 on 2019/12/03.
+//  Copyright © 2019 深瀬 貴将. All rights reserved.
+//
+
+import XCTest
+@testable import APIpractice2
+
+class APIpractice2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
