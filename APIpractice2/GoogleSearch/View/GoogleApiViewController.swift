4823620101a8eb482726229c7ba2eee291aa782a
深瀬 貴将 <fukase@arsaga.jp>
2020-01-23 15:55:15 +0900

    tableViewへの表示をRxじゃない書き方に戻して全件表示される様にした。

--- APIpractice2/GoogleSearch/View/GoogleApiViewController.swift
+++ APIpractice2/GoogleSearch/View/GoogleApiViewController.swift
@@ -20,17 +20,20 @@ class GoogleApiViewController: UIViewController, StoryboardInstantiatable {
     let disposeBag = DisposeBag()
     let viewModel: GoogleViewModelType = GoogleViewModel()
     
+    var titles:[String] = []
+    var links:[String] = []
     
-    let dataSource = RxTableViewSectionedReloadDataSource<GoogleDataSource>(configureCell: { dataSource, tableView, indexPath, item in
+    
+    //let dataSource = RxTableViewSectionedReloadDataSource<GoogleDataSource>(configureCell: { dataSource, tableView, indexPath, item in
                 
-        let cell = TableViewUtil.createCell(tableView, identifier: GoogleApiCell.reusableIdentifier, indexPath) as! GoogleApiCell
-        print(item)
-        let title = item.items[indexPath.row].title
-        let link = item.items[indexPath.row].link
-        print(indexPath)
-        cell.googleBindData(title: title, link: link)
-        return cell
-    })
+    //    let cell = TableViewUtil.createCell(tableView, identifier: GoogleApiCell.reusableIdentifier, indexPath) as! GoogleApiCell
+    //    print(item)
+    //    let title = item.items[indexPath.row].title
+    //    let link = item.items[indexPath.row].link
+    //    print(indexPath)
+    //    cell.googleBindData(title: title, link: link)
+    //    return cell
+    //})
     
     //var query: String?
     //var startIndex = 1
@@ -43,19 +46,32 @@ class GoogleApiViewController: UIViewController, StoryboardInstantiatable {
         super.viewDidLoad()
 
         TableViewUtil.registerCell(tableView, identifier: GoogleApiCell.reusableIdentifier)
+        tableView.delegate = self
+        tableView.dataSource = self
         searchTextField?.delegate = self
         
         viewModel.outputs.articles
-            //.subscribe(onNext: { element in
-            //    print("VC: elementの中身:\(element)")
-            //    print(element.count)
-            //    print(element[0].items.count)
-            //    print(element[0].items[0].items[0].title)
-            //}).disposed(by: disposeBag)
-            .bind(to: tableView.rx.items(dataSource: dataSource))
-            .disposed(by: disposeBag)
+            .subscribe(onNext: { element in
+                
+                for titleAndLink in element[0].items[0].items {
+                    self.titles += [titleAndLink.title]
+                    self.links += [titleAndLink.link]
+                }
+                self.tableView.reloadData()
+                
+                print("VC: elementの中身:\(element)")
+                print(element.count)
+                print(element[0].items.count)
+                print(element[0].items[0].items[0].title)
+                print(element[0].items[0].items.count)
+                print(self.titles)
+                print(self.links)
+                
+            }).disposed(by: disposeBag)
+            //.bind(to: tableView.rx.items(dataSource: dataSource))
+            //.disposed(by: disposeBag)
         
-        tableView.rx.setDelegate(self).disposed(by: disposeBag)
+        //tableView.rx.setDelegate(self).disposed(by: disposeBag)
 
     }
     
@@ -103,13 +119,12 @@ class GoogleApiViewController: UIViewController, StoryboardInstantiatable {
 }
 
 
-extension GoogleApiViewController: UITableViewDelegate {
+extension GoogleApiViewController: UITableViewDelegate, UITableViewDataSource {
     
      func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return self.titles.count
      }
 
-     
      func tableView(_ tabbleView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
          //if let height = self.cellHeightsDictionary[indexPath] {
           //   return height
@@ -120,6 +135,15 @@ extension GoogleApiViewController: UITableViewDelegate {
      func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
          return 130
      }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = TableViewUtil.createCell(tableView, identifier: GoogleApiCell.reusableIdentifier, indexPath) as! GoogleApiCell
+        let title = self.titles[indexPath.row]
+        let link = self.links[indexPath.row]
+        cell.googleBindData(title: title, link: link)
+        return cell
+    }
+    
 }
 
 extension GoogleApiViewController: UITextFieldDelegate {
