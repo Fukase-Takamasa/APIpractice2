f572a09db1411fa8a84e500d4904ce7007ebb6c6
深瀬 貴将 <fukase@arsaga.jp>
2020-01-24 20:38:58 +0900

    セルタップ時のメソッドをrxで書いてみた。（tap検知はできたが、articleの中身は取得できていない）

--- APIpractice2/GoogleSearch/View/GoogleApiViewController.swift
+++ APIpractice2/GoogleSearch/View/GoogleApiViewController.swift
@@ -57,12 +57,27 @@ class GoogleApiViewController: UIViewController, StoryboardInstantiatable {
             .bind(to: viewModel.inputs.searchButtonTapped)
             .disposed(by: disposeBag)
         
+        //output
         viewModel.outputs.articles
             .bind(to: tableView.rx.items(dataSource: dataSource))
             .disposed(by: disposeBag)
         
-        //output
+        //other
         tableView.rx.setDelegate(self).disposed(by: disposeBag)
+        
+        tableView.rx.itemSelected
+            .subscribe(onNext: { [unowned self] indexPath in
+                print("Cell tapped")
+                print(indexPath)
+                self.viewModel.outputs.articles
+                    .subscribe(onNext: { element in
+                        print("elementの中身\(element)")
+                        print(element[0])
+                        print(element[0].items)
+                        //let vc = ArticleViewController.instantiate()
+                        //vc.articleTitle = element
+                    }).disposed(by: self.disposeBag)
+            }).disposed(by: disposeBag)
 
     }
 }
