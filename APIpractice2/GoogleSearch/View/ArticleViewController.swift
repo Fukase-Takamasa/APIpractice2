72f3362fe8128e294d96fb2f678b395fcda60473
深瀬 貴将 <fukase@arsaga.jp>
2020-01-24 19:45:26 +0900

    セルタップで表示する記事のVCを追加。

---
+++ APIpractice2/GoogleSearch/View/ArticleViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ArticleViewController.swift
+//  APIpractice2
+//
+//  Created by 深瀬 貴将 on 2020/01/24.
+//  Copyright © 2020 深瀬 貴将. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import Instantiate
+import InstantiateStandard
+
+
+class ArticleViewController: UIViewController, StoryboardInstantiatable {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
