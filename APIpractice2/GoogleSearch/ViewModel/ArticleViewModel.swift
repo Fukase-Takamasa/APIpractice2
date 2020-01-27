3b15a3973641b795de2aa1ac80e8b439dc052f86
深瀬 貴将 <fukase@arsaga.jp>
2020-01-27 17:21:09 +0900

    ArticleViewのVMを追加。ベースの記述。

---
+++ APIpractice2/GoogleSearch/ViewModel/ArticleViewModel.swift
@@ -0,0 +1,54 @@
+//
+//  ArticleViewModel.swift
+//  APIpractice2
+//
+//  Created by 深瀬 貴将 on 2020/01/27.
+//  Copyright © 2020 深瀬 貴将. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+protocol ArticleViewModelInputs {
+    
+}
+
+protocol ArticleViewModelOutputs {
+    <#requirements#>
+}
+
+protocol ArticleViewModelType {
+    var inputs: ArticleViewModelInputs {get}
+    var outputs: ArticleViewModelOutputs {get}
+}
+
+class ArticleViewModel: ArticleViewModelInputs, ArticleViewModelOutputs {
+    //input
+    
+    
+    //output
+    
+    
+    //other
+    private let scheduler: SchedulerType
+    private let disposeBag = DisposeBag()
+    
+    init(scheduler: SchedulerType = ConcurrentMainScheduler.instance) {
+        //other
+        self.scheduler = scheduler
+        
+        //output
+        
+        
+        //input
+        
+        
+        
+    }
+}
+
+extension ArticleViewModel: ArticleViewModelType {
+    var inputs: ArticleViewModelInputs { return self}
+    var outputs: ArticleViewModelOutputs { return self}
+}
